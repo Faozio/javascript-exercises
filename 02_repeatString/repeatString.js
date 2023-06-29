d3d498e98594249a0d5f60390306e21dfd269ce2
Faozio <135945136+Faozio@users.noreply.github.com>
2023-06-29 20:03:16 +0100

    Completed 01, 02 and 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0) return "ERROR";
+    let aswear = "";
+    for(let i = 0; i < num; i++){
+        aswear += str;
+    }
+    return aswear;
 };
 
 // Do not edit below this line
