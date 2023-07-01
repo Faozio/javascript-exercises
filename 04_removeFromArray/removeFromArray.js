3ded34996b1832978b072d53fb48a28b14fe20e4
Faozio <135945136+Faozio@users.noreply.github.com>
2023-07-01 01:45:47 +0100

    Completed 04, 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(items => !args.includes(items))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    const small = Math.min(num1, num2);
+    const big = Math.max(num1, num2);
+    let sum = 0;
+    for (let i = small; i <= big; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
