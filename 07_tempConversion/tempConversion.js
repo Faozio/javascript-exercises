f9e1ad28b52395c2398f5a2a17e90eb13417c6b9
Faozio <135945136+Faozio@users.noreply.github.com>
2023-07-01 18:39:27 +0100

    Completed 06, 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  f = (((f - 32) * (5 / 9) * 10) / 10).toFixed(1);
+  return Number(f);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  c = ((((c * 9) / 5 + 32) * 10) / 10).toFixed(1);
+  return Number(c);
 };
 
 // Do not edit below this line
